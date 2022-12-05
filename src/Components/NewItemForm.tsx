d35f385535713772250dca085e0dac28b2bb3084
Rachidi Judicaël <wahab@MacBook-Pro-de-Rachidi.local>
2022-12-05 16:42:57 +0400

    feat:automatically focus on an input

--- src/Components/NewItemForm.tsx
+++ src/Components/NewItemForm.tsx
@@ -4,6 +4,7 @@ import {
   NewItemFormContainer,
   NewItemInput,
 } from '../styles/styles'
+import { useFocus } from '../utils/useFocus'
 
 type NewItemFormProps = {
   onAdd(text: string): void
@@ -11,11 +12,15 @@ type NewItemFormProps = {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState('')
-
+  const inputRef = useFocus()
   return (
     <NewItemFormContainer>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
-      <NewItemButton onClick={() => onAdd(text )}>Create</NewItemButton>
+      <NewItemInput
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        ref={inputRef }
+      />
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   )
 }
